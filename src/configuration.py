6659dc28ace8307991783d37baa8aa192fd937c7
h3ne58 <grinkiko@gmail.com>
2025-02-20 09:39:36 -0300

    add defaults

--- src/configuration.py
+++ src/configuration.py
@@ -1,12 +1,14 @@
+from typing import Optional
+
 from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
 class AppSettings(BaseSettings):
-    database_url: str
-    postgres_user: str
-    postgres_password: str
-    postgres_db: str
+    database_url: Optional[str] = Field('postgresql+asyncpg://myuser:mypassword@database:5432/mydatabase')
+    postgres_user: Optional[str] = Field('myuser')
+    postgres_password: Optional[str] = Field('mypassword')
+    postgres_db: Optional[str] = Field('mydatabase')
 
     class Config:
         env_file = "../.env_example"
